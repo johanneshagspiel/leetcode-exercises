3608f2ce735bf99794768de19ac1b4363d7aeb19
jhagspiel <j.hagspiel@student.tudelft.nl>
2022-11-06 11:26:18 +0100

    Added newest exercises - 452 done.

---
+++ python/daily_exercises/899 Orderly Queue/attempt_1.py
@@ -0,0 +1,16 @@
+class Solution:
+    def orderlyQueue(self, s: str, k: int) -> str:
+
+        if k == 1:
+
+            options = [s]
+            s_list = list(s)
+            for _ in range(len(s)):
+                pos_0 = s_list.pop(0)
+                s_list.append(pos_0)
+                options.append("".join(s_list))
+
+            return min(options)
+
+        else:
+            return "".join(sorted(s))
